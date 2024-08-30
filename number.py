42674ceaff6a08444b30e8873410ba81dc571ab7
jsline59 <68678970+jsline59@users.noreply.github.com>
2024-08-30 16:26:52 +0900

    Update number.py

--- number.py
+++ number.py
@@ -3,8 +3,12 @@ e = 0
 answer = ''
 while e != 3:
     num = input()
+    if int(num) < 0:
+        #음수일때
+        pass
     if not num.replace('-','').isdigit() or len(num) > 20:
         e += 1
+        print("Input type error")
         continue
     for i,v in enumerate(num[:: -1]):
         if i % 3 == 0 and i != 0:
